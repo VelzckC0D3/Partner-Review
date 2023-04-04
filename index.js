e62d53c9fd482ecaa4c199e4e241cb0ffc3e5353
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-04-03 22:54:42 -0500

    Index
    - Template for coding review

---
+++ index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import List from './modules/list.js';
+
+const list = new List();
+list.loadTask();
+list.addTask();
+list.removeTask();
+list.editTask();
+list.showList();
+list.draggables();
